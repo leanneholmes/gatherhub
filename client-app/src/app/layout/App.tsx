2521d8ca851705c4d8c34aae25c0b6a17a745e26
Leanne Holmes <lholmes24@my.bcit.ca>
2023-08-21 01:58:04 -0700

    Added NavBar and Activity List Dashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,25 +1,28 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { Header, List } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
+import { Activity } from "../models/activty";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then((response) => {
-      setActivities(response.data);
-    });
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
   }, []);
 
   return (
-    <div>
-      <Header as="h2" icon="users" content="SociaLink" />
-      <List>
-        {activities.map((activity: any) => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+    <>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard activities={activities} />
+      </Container>
+    </>
   );
 }
 
