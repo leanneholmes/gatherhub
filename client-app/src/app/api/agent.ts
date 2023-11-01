18d2fbe113c61646038d0617d4ac2536a3da6034
Leanne Holmes <lholmes24@my.bcit.ca>
2023-10-31 23:43:49 -0700

    Formik replacing Semantic UI for forms

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -21,10 +21,7 @@ axios.interceptors.response.use(
     const { data, status, config } = error.response as AxiosResponse;
     switch (status) {
       case 400:
-        if (
-          config.method === "get" &&
-          Object.prototype.hasOwnProperty.call(data.errors, "id")
-        ) {
+        if (config.method === "get" && data.errors.hasOwnProperty("id")) {
           router.navigate("/not-found");
         }
         if (data.errors) {
